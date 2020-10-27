1853d41edf27d0d32e436c092188fd16a520f952
Anh Le <letuananh.ttmmt.ctu@gmail.com>
2020-10-27 09:53:20 +0700

    Update sample

--- cmd/go-mod-example/main.go
+++ cmd/go-mod-example/main.go
@@ -3,11 +3,9 @@ package main
 import (
 	"fmt"
 
-	greetname "github.com/captain-corgi/go-mod-example/greet"
-	"github.com/quii/go-mod-example/greet"
+	"github.com/captain-corgi/go-mod-example/greet"
 )
 
 func main() {
-	fmt.Println(greetname.HelloHuman("Anh Le"))
-	fmt.Println(greet.TheWorld())
+	fmt.Println(greet.HelloHuman("Predator"))
 }
